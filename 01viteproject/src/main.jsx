2ddb5ca88038ac48230c7403b2aedfcdeaaa52fe
Shubham Maji <122548421+shubhammaji1@users.noreply.github.com>
2024-04-27 13:06:35 +0000

    React

--- 01viteproject/src/main.jsx
+++ 01viteproject/src/main.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(<div>
+          <h1>Custom React</h1>
+              </div>)
+    
+  
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MyApp/>
   </React.StrictMode>,
 )
