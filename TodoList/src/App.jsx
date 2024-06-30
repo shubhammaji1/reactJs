b64b77a228904ffeee1bd82835a6826f84d5d8d4
Shubham Maji <122548421+shubhammaji1@users.noreply.github.com>
2024-06-30 17:05:42 +0000

    TODO

--- TodoList/src/App.jsx
+++ TodoList/src/App.jsx
@@ -40,7 +40,20 @@ function App() {
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos))
   }, [todos])
+
+  const [dateTime, setDateTime] = useState("");
   
+useEffect(() => {
+  const interval = setInterval(()=>{
+    const date = new Date();
+    const formatedDate = date.toLocaleDateString();
+    const formatedTime = date.toLocaleTimeString();
+    setDateTime(`${formatedDate}-${formatedTime}`)
+  },1000)
+
+    return ()=> clearInterval(interval);
+  
+}, [])
 
 
 
@@ -48,7 +61,8 @@ function App() {
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Your Todos</h1>
+                    <h1 className='date'>{dateTime}</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
                         <TodoForm />
