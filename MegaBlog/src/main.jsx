5ce9ab91b435466262e2ffb3eca03fb4f5d39de9
Shubham Maji <122548421+shubhammaji1@users.noreply.github.com>
2024-07-28 17:32:29 +0000

    Megablog

--- MegaBlog/src/main.jsx
+++ MegaBlog/src/main.jsx
@@ -5,17 +5,17 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home.jsx'
+// import Home from './pages/Home.jsx'
 import { AuthLayout, Login } from './components/index.js'
 
 
-import AddPost from "./pages/AddPost";
-import Signup from './pages/Signup'
-import EditPost from "./pages/EditPost";
+// import AddPost from "./pages/AddPost";
+// import Signup from './pages/Signup'
+// import EditPost from "./pages/EditPost";
 
-import Post from "./pages/Post";
+// import Post from "./pages/Post";
 
-import AllPosts from "./pages/AllPosts";
+// import AllPosts from "./pages/AllPosts";
 
 const router = createBrowserRouter([
   {
