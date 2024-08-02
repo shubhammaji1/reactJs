0a335676a1d08924957fe98e5123820fcfc7fd28
Shubham Maji <122548421+shubhammaji1@users.noreply.github.com>
2024-08-02 13:10:02 +0000

    curr

--- Currencyconverter/src/App.jsx
+++ Currencyconverter/src/App.jsx
@@ -28,6 +28,7 @@ function App() {
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
         backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
+
       }}
     >
       <div className="w-full">
